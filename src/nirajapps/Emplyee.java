5788eb8208c8d87af53f63098eb1cf9f99d466c1
Niraj Singh <Niraj Singh@192.168.0.8>
2018-09-06 02:30:02 +0530

    1st file change

--- src/nirajapps/Emplyee.java
+++ src/nirajapps/Emplyee.java
@@ -1,5 +1,27 @@
 package nirajapps;
 
 public class Emplyee {
+	
+	private int empid;
+	private String empName;
+	@Override
+	public String toString() {
+		return "Emplyee [empid=" + empid + ", empName=" + empName + "]";
+	}
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Emplyee() {
+		super();
+	}
 
 }
